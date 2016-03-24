ed0fffc281fec720ec33bacd1a8bfa10d0ee6e93
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-03-24 16:57:25 -0400

    Added files via upload

---
+++ js/OMSload.js
@@ -0,0 +1,274 @@
+﻿var tryingToReconnect = false, user;
+
+$(document).ready(function () {
+
+    alert(navigator.network.connection.type.toString());
+
+    //var status = checkNetworkConnection();
+    //if (status != "none") {
+
+    //}
+
+    checkCookie();
+
+    getSpinner();
+    $("#spinCont").hide();
+
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "0",
+        "hideDuration": "0",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    $.connection.hub.url = "http://gis.fourcty.org/FCEMCrest/signalr/hubs";
+
+    $.connection.hub.logging = true;
+
+    var mainChat = $.connection.mainHub;
+    mainChat.client.broadcastMessage = function (data, option) {
+
+        if (tryingToReconnect)  //catch in case reconnected doesn't get called
+        {
+            tryingToReconnect = false;
+        }
+
+        switch (option) {
+            case "AVL":
+                AVLResults(data);
+                break;
+            case "OUTAGE":
+                listOutages(data);
+                break;
+            case "SCADA":
+                //processSCADA(data);
+                break;
+            default:
+        }
+    };
+
+    $.connection.hub.start().done(function () {
+        //init();  //intalize after login is validated
+    });
+
+    $.connection.hub.disconnected(function () {
+        if (tryingToReconnect) {
+            setTimeout(function () {
+                $.connection.hub.start().done(function () { init(); });
+            }, 5000); // Restart connection after 5 seconds.
+        }
+    });
+
+    $.connection.hub.reconnecting(function () {
+        toastr["error"]("Network connection lost! Trying to restore connection...");
+        tryingToReconnect = true;
+    });
+
+    $.connection.hub.reconnected(function () {
+        toastr["success"]("Connection restored!");
+        tryingToReconnect = false;
+    });
+
+});
+
+//region Login&Cookies
+function checkLogin() {
+    user = $("#un").val().trim();
+    var _pw = $("#pw").val().trim();
+
+    var paramItems = user + "|" + _pw;
+    $.ajax({
+        type: "GET",
+        url: "http://gis.fourcty.org/FCEMCrest/FCEMCDataService.svc/authenticateYouSir/" + paramItems,
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        success: function (results) {
+            if (results.authenticateYouSirResult) {
+                $("#loginError").text("");
+                $.mobile.pageContainer.pagecontainer("change", "#page1");
+                $("#spinCont").show();
+
+                if (localStorage.fcemcOMS_uname == undefined) {
+                    setCookie(user, _pw, 20); //expires 20 days from inital login
+                }
+
+                init();
+                getOutages();
+
+            }
+            else {
+                window.localStorage.clear();
+                $("#loginError").text("Login Unsucessful");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            var e = errorThrown;
+        }
+    });
+}
+
+function setCookie(u, p, t) {
+    window.localStorage.clear();
+    localStorage.setItem("fcemcOMS_uname", u);
+    localStorage.setItem("fcemcOMS_pass", p);
+    var d = new Date();
+    d.setDate(d.getDate() + t);
+    localStorage.setItem("fcemcOMS_timeout", d);
+}
+
+function getCookie() {    
+    var isCookies = false;
+    if (localStorage.fcemcOMS_uname != null && localStorage.fcemcOMS_pass != null)
+    {
+        isCookies = true;
+    }    
+    return isCookies;
+}
+
+function checkCookie() {
+    var valid = getCookie();
+    if (valid == true) {        
+        if (new Date(localStorage.fcemcOMS_timeout) > new Date()) {
+            $("#un").val(localStorage.fcemcOMS_uname);
+            $("#pw").val(localStorage.fcemcOMS_pass);
+        }
+    }
+}
+//endregion
+
+//function checkNetworkConnection() {
+
+//    var networkState = navigator.network.connection.type;
+
+//    var states = {};
+//    states[Connection.UNKNOWN] = 'Unknown connection';
+//    states[Connection.ETHERNET] = 'Ethernet connection';
+//    states[Connection.WIFI] = 'WiFi connection';
+//    states[Connection.CELL_2G] = 'Cell 2G connection';
+//    states[Connection.CELL_3G] = 'Cell 3G connection';
+//    states[Connection.CELL_4G] = 'Cell 4G connection';
+//    states[Connection.NONE] = 'No network connection';
+
+//    alert('Connection type: ' + states[networkState]);
+
+//    return networkState;
+//}
+
+function init() {
+    $.ajax({
+        type: "GET",
+        url: "http://gis.fourcty.org/FCEMCrest/FCEMCDataService.svc/initalizeLink",
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        success: function (results) {
+            var r = results;
+            //Everthing is ok, server side code initailized        
+            //var r = results;
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            var t = textStatus;
+            var e = errorThrown;
+            //alert(errorThrown);
+            //var t = textStatus;
+        }
+    });
+}
+
+function getOutages() {
+    $.ajax({
+        type: "GET",
+        url: "http://gis.fourcty.org/FCEMCrest/FCEMCDataService.svc/getOUTAGECASES",
+        contentType: "application/json; charset=utf-8",
+        cache: false,
+        success: function (results) {
+            listOutages(results.getOUTAGECASESResult);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            var e = errorThrown;
+        }
+    });
+}
+
+function listOutages(data) {
+    if (data.length > 0) {
+        var _string = "<div data-role='collapsible-set'>";
+        for (i = 0; i < data.length; i++) {
+            _string += "<div data-role='collapsible'><h3>" + data[i].CASENUM + "</h3>";
+            _string += "<div class='accdEntry'><b>Customer Count:</b> " + data[i].CUSTCOUNT + "</div>";
+            _string += "<div class='accdEntry'><b>Assigned To:</b> " + data[i].ASSIGNEDTO + "</div>";
+            _string += "<div class='accdEntry'><b>Start Time:</b> " + data[i].TIMESTRT + "</div>";
+            _string += "<div class='accdEntry'><b>Start Date:</b> " + data[i].DATESTRT + "</div>";
+            _string += "<div class='accdEntry'><b>Element:</b> " + data[i].ELEMENT + "</div>";
+            _string += "<div class='accdEntry'><b>Element ID:</b> " + data[i].ELEMENTID + "</div>";
+            _string += "<div class='accdEntry'><b>Pole Number:</b> " + data[i].POLENUM + "</div>";
+            _string += "<div class='accdEntry'><b>Case Status:</b> " + data[i].CASESTATUS + "</div>";
+            _string += "</div>";
+        }
+        _string += "</div>";
+
+        $("#outage").html("");
+        $("#outage").html(_string.toString());
+        $('#outage [data-role=collapsible-set]').collapsibleset();
+
+        navigator.notification.beep(2);
+        navigator.notification.vibrate(1000);
+    }
+    else if (data.length == 0) {
+        $("#outage").html("No Outages at this time");
+    }
+    $("#spinCont").hide();
+}
+
+function getSpinner() {
+    var opts = {
+        lines: 12             // The number of lines to draw
+        , length: 7             // The length of each line
+        , width: 5              // The line thickness
+        , radius: 10            // The radius of the inner circle
+        , scale: 1.0            // Scales overall size of the spinner
+        , corners: 1            // Roundness (0..1)
+        , color: '#000'         // #rgb or #rrggbb
+        , opacity: 1 / 4          // Opacity of the lines
+        , rotate: 0             // Rotation offset
+        , direction: 1          // 1: clockwise, -1: counterclockwise
+        , speed: 1              // Rounds per second
+        , trail: 100            // Afterglow percentage
+        , fps: 20               // Frames per second when using setTimeout()
+        , zIndex: 2e9           // Use a high z-index by default
+        , className: 'spinner'  // CSS class to assign to the element
+        , top: '50%'            // center vertically
+        , left: '50%'           // center horizontally
+        , shadow: false         // Whether to render a shadow
+        , hwaccel: false        // Whether to use hardware acceleration (might be buggy)
+        , position: 'absolute'  // Element positioning
+    }
+    var target = document.getElementById('spinwheel');
+    spinner = new Spinner(opts).spin(target);
+}
+
+function AVLResults(data) {
+    if (data.length > 0) {
+        var _string = "";
+        for (i = 0; i < data.length; i++) {
+            _string += "<div>" + data[i].VID + ": " + data[i].LOG_DATETIME + "</div>";
+        }
+
+        $("#avl").html("");
+        $("#avl").html(_string.toString());
+    }
+    else if (data.length == 0) {
+        $("#avl").html("No AVL at this time");
+    }
+    $("#spinCont").hide();
+}
