96b6d3eef84455d6f330f435a2942663e00c5a28
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-03-28 14:59:20 -0400

    Added files via upload

--- js/OMSload.js
+++ js/OMSload.js
@@ -113,6 +113,7 @@ function checkLogin() {
         },
         error: function (jqXHR, textStatus, errorThrown) {
             var e = errorThrown;
+            $("#loginError").text("Login Unsucessful");
         }
     });
 }
@@ -144,24 +145,6 @@ function checkCookie() {
     }
 }
 //endregion
-
-//function checkNetworkConnection() {
-
-//    var networkState = navigator.network.connection.type;
-
-//    var states = {};
-//    states[Connection.UNKNOWN] = 'Unknown connection';
-//    states[Connection.ETHERNET] = 'Ethernet connection';
-//    states[Connection.WIFI] = 'WiFi connection';
-//    states[Connection.CELL_2G] = 'Cell 2G connection';
-//    states[Connection.CELL_3G] = 'Cell 3G connection';
-//    states[Connection.CELL_4G] = 'Cell 4G connection';
-//    states[Connection.NONE] = 'No network connection';
-
-//    alert('Connection type: ' + states[networkState]);
-
-//    return networkState;
-//}
 
 function init() {
     $.ajax({
