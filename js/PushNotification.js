1276d142bdc2186351aaf265da1f077304370457
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-04-14 13:58:56 -0400

    Added files via upload

--- js/PushNotification.js
+++ js/PushNotification.js
@@ -1,249 +1,60 @@
-﻿/* global cordova:false */
-/* globals window */
+﻿//
+//  PushNotification.js
+//
+// Created by Olivier Louvignes on 06/05/12.
+// Inspired by Urban Airship Inc orphaned PushNotification phonegap plugin.
+//
+// Copyright 2012 Olivier Louvignes. All rights reserved.
+// MIT Licensed
 
-/*!
- * Module dependencies.
- */
+(function (cordova) {
 
-//var exec = cordova.require('cordova/exec');
+    function PushNotification() { }
 
-/**
- * PushNotification constructor.
- *
- * @param {Object} options to initiate Push Notifications.
- * @return {PushNotification} instance that can be monitored and cancelled.
- */
-
-var PushNotification = function (options) {
-    this._handlers = {
-        'registration': [],
-        'notification': [],
-        'error': []
+    // Call this to register for push notifications and retreive a deviceToken
+    PushNotification.prototype.registerDevice = function (config, callback) {
+        console.log("REGISTERING IN JS");
+        cordova.exec(callback, callback, "PushNotification", "registerDevice", config ? [config] : []);
     };
 
-    // require options parameter
-    if (typeof options === 'undefined') {
-        throw new Error('The options argument is required.');
-    }
-
-    // store the options to this object instance
-    this.options = options;
-
-    // triggered on registration and notification
-    var that = this;
-    var success = function (result) {
-        if (result && typeof result.registrationId !== 'undefined') {
-            that.emit('registration', result);
-        } else if (result && result.additionalData && typeof result.additionalData.callback !== 'undefined') {
-            var executeFunctionByName = function (functionName, context /*, args */) {
-                var args = Array.prototype.slice.call(arguments, 2);
-                var namespaces = functionName.split('.');
-                var func = namespaces.pop();
-                for (var i = 0; i < namespaces.length; i++) {
-                    context = context[namespaces[i]];
-                }
-                return context[func].apply(context, args);
-            };
-
-            executeFunctionByName(result.additionalData.callback, window, result);
-        } else if (result) {
-            that.emit('notification', result);
-        }
+    // Call this to retreive pending notification received while the application is in background or at launch
+    PushNotification.prototype.getPendingNotifications = function (callback) {
+        cordova.exec(callback, callback, "PushNotification", "getPendingNotifications", []);
     };
 
-    // triggered on error
-    var fail = function (msg) {
-        var e = (typeof msg === 'string') ? new Error(msg) : msg;
-        that.emit('error', e);
+    // Call this to get a detailed status of remoteNotifications
+    PushNotification.prototype.getRemoteNotificationStatus = function (callback) {
+        console.log("Callback is " + callback);
+        cordova.exec(callback, callback, "PushNotification", "getRemoteNotificationStatus", []);
     };
 
-    // wait at least one process tick to allow event subscriptions
-    setTimeout(function () {
-        exec(success, fail, 'PushNotification', 'init', [options]);
-    }, 10);
-};
-
-/**
- * Unregister from push notifications
- */
-
-PushNotification.prototype.unregister = function (successCallback, errorCallback, options) {
-    if (!errorCallback) { errorCallback = function () { }; }
-
-    if (typeof errorCallback !== 'function') {
-        console.log('PushNotification.unregister failure: failure parameter not a function');
-        return;
-    }
-
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.unregister failure: success callback parameter must be a function');
-        return;
-    }
-
-    var that = this;
-    var cleanHandlersAndPassThrough = function () {
-        if (!options) {
-            that._handlers = {
-                'registration': [],
-                'notification': [],
-                'error': []
-            };
-        }
-        successCallback();
+    // Call this to set the application icon badge
+    PushNotification.prototype.setApplicationIconBadgeNumber = function (badge, callback) {
+        cordova.exec(callback, callback, "PushNotification", "setApplicationIconBadgeNumber", [{ badge: badge }]);
     };
 
-    exec(cleanHandlersAndPassThrough, errorCallback, 'PushNotification', 'unregister', [options]);
-};
+    // Call this to clear all notifications from the notification center
+    PushNotification.prototype.cancelAllLocalNotifications = function (callback) {
+        cordova.exec(callback, callback, "PushNotification", "cancelAllLocalNotifications", []);
+    };
 
-/**
- * Call this to set the application icon badge
- */
+    // Call this to retreive the original device unique id
+    // @warning As of today, usage is deprecated and requires explicit consent from the user
+    PushNotification.prototype.getDeviceUniqueIdentifier = function (callback) {
+        cordova.exec(callback, callback, "PushNotification", "getDeviceUniqueIdentifier", []);
+    };
 
-PushNotification.prototype.setApplicationIconBadgeNumber = function (successCallback, errorCallback, badge) {
-    if (!errorCallback) { errorCallback = function () { }; }
+    // Event spawned when a notification is received while the application is active
+    PushNotification.prototype.notificationCallback = function (notification) {
+        var ev = document.createEvent('HTMLEvents');
+        ev.notification = notification;
+        ev.initEvent('push-notification', true, true, arguments);
+        document.dispatchEvent(ev);
+    };
 
-    if (typeof errorCallback !== 'function') {
-        console.log('PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function');
-        return;
-    }
+    cordova.addConstructor(function () {
+        if (!window.plugins) window.plugins = {};
+        window.plugins.pushNotification = new PushNotification();
+    });
 
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function');
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'PushNotification', 'setApplicationIconBadgeNumber', [{ badge: badge }]);
-};
-
-/**
- * Get the application icon badge
- */
-
-PushNotification.prototype.getApplicationIconBadgeNumber = function (successCallback, errorCallback) {
-    if (!errorCallback) { errorCallback = function () { }; }
-
-    if (typeof errorCallback !== 'function') {
-        console.log('PushNotification.getApplicationIconBadgeNumber failure: failure parameter not a function');
-        return;
-    }
-
-    if (typeof successCallback !== 'function') {
-        console.log('PushNotification.getApplicationIconBadgeNumber failure: success callback parameter must be a function');
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'PushNotification', 'getApplicationIconBadgeNumber', []);
-};
-
-/**
- * Listen for an event.
- *
- * The following events are supported:
- *
- *   - registration
- *   - notification
- *   - error
- *
- * @param {String} eventName to subscribe to.
- * @param {Function} callback triggered on the event.
- */
-
-PushNotification.prototype.on = function (eventName, callback) {
-    if (this._handlers.hasOwnProperty(eventName)) {
-        this._handlers[eventName].push(callback);
-    }
-};
-
-/**
- * Remove event listener.
- *
- * @param {String} eventName to match subscription.
- * @param {Function} handle function associated with event.
- */
-
-PushNotification.prototype.off = function (eventName, handle) {
-    if (this._handlers.hasOwnProperty(eventName)) {
-        var handleIndex = this._handlers[eventName].indexOf(handle);
-        if (handleIndex >= 0) {
-            this._handlers[eventName].splice(handleIndex, 1);
-        }
-    }
-};
-
-/**
- * Emit an event.
- *
- * This is intended for internal use only.
- *
- * @param {String} eventName is the event to trigger.
- * @param {*} all arguments are passed to the event listeners.
- *
- * @return {Boolean} is true when the event is triggered otherwise false.
- */
-
-PushNotification.prototype.emit = function () {
-    var args = Array.prototype.slice.call(arguments);
-    var eventName = args.shift();
-
-    if (!this._handlers.hasOwnProperty(eventName)) {
-        return false;
-    }
-
-    for (var i = 0, length = this._handlers[eventName].length; i < length; i++) {
-        this._handlers[eventName][i].apply(undefined, args);
-    }
-
-    return true;
-};
-
-PushNotification.prototype.finish = function (successCallback, errorCallback, id) {
-    if (!successCallback) { successCallback = function () { }; }
-    if (!errorCallback) { errorCallback = function () { }; }
-    if (!id) { id = 'handler'; }
-
-    if (typeof successCallback !== 'function') {
-        console.log('finish failure: success callback parameter must be a function');
-        return;
-    }
-
-    if (typeof errorCallback !== 'function') {
-        console.log('finish failure: failure parameter not a function');
-        return;
-    }
-
-    exec(successCallback, errorCallback, 'PushNotification', 'finish', [id]);
-};
-
-/*!
- * Push Notification Plugin.
- */
-
-module.exports = {
-    /**
-     * Register for Push Notifications.
-     *
-     * This method will instantiate a new copy of the PushNotification object
-     * and start the registration process.
-     *
-     * @param {Object} options
-     * @return {PushNotification} instance
-     */
-
-    init: function (options) {
-        return new PushNotification(options);
-    },
-
-    hasPermission: function (successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'PushNotification', 'hasPermission', []);
-    },
-
-    /**
-     * PushNotification Object.
-     *
-     * Expose the PushNotification object for direct use
-     * and testing. Typically, you should use the
-     * .init helper method.
-     */
-
-    PushNotification: PushNotification
-};
+})(window.cordova || window.Cordova || window.PhoneGap);
