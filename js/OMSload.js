7d686bc4fd0a05a81da92d0c80dcacca2bc399c4
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-04-14 12:03:46 -0400

    Added files via upload

--- js/OMSload.js
+++ js/OMSload.js
@@ -9,6 +9,36 @@ $(document).ready(function () {
         $(".pg").css({ "margin-top": "20px" });
     }
 
+    var push = PushNotification.init({
+        "android": { "senderID": "12345679" },
+        "ios": {
+            "alert": "true",
+            "badge": "true",
+            "sound": "true"
+        },
+        "windows": {}
+    });
+
+    push.on('registration', function (data) {
+        var _d = data;
+        // data.registrationId 
+    });
+
+    push.on('notification', function (data) {
+        var _d = data;
+        // data.message, 
+        // data.title, 
+        // data.count, 
+        // data.sound, 
+        // data.image, 
+        // data.additionalData 
+    });
+
+    push.on('error', function (e) {
+        var _e = e;
+        // e.message 
+    });
+
 
 
     if (navigator.onLine) {
@@ -45,6 +75,7 @@ $(document).ready(function () {
         mainChat.client.broadcastMessage = function (data, option) {
 
             cordova.plugins.notification.badge.set(badgeCount += 1);
+            
 
             console.log("Test");
 
