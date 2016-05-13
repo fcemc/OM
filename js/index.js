c60c557ae7d1d967d3dffa6ea5fb275e56d642ae
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-05-13 16:08:02 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -68,7 +68,7 @@ function onNotificationAPN(e) {
     if (e.sound) {
         // playing a sound also requires the org.apache.cordova.media plugin
         //var snd = new Media(e.sound);
-        var snd = new Media("Swing02.WAV");
+        var snd = new Media("FCEMCsound.WAV");
         snd.play();
     }
 
@@ -110,7 +110,7 @@ function onNotification(e) {
                 // if the notification contains a soundname, play it.
                 // playing a sound also requires the org.apache.cordova.media plugin
                 //var my_media = new Media("/android_asset/www/" + soundfile);
-                var my_media = new Media("/android_asset/www/Swing02.WAV");
+                var my_media = new Media("/android_asset/www/FCEMCsound.WAV");
                 my_media.play();
             }
             else {	// otherwise we were launched because the user touched a notification in the notification tray.
