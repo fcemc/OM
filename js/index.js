1850a72c276d31c04c263cf897b396ba95efa9e5
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-05-13 15:04:47 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -119,8 +119,7 @@ function onNotification(e) {
                     var e = e.coldstart;
             }
 
-            pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, e.payload.msgcnt);  //doesn't work
-
+            cordova.plugins.notification.badge.set(e.payload.msgcnt);
 
             //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
             ////android only
